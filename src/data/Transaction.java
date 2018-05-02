f9fa44cd21fb7ad1f7ea4dc1f7e3fb47a8b59685
haoxiangma <mahomehi@126.com>
2018-05-02 15:46:22 +0800

    1. Transaction

---
+++ src/data/Transaction.java
@@ -0,0 +1,79 @@
+package data;
+
+/**
+ * Transaction data.
+ */
+public class Transaction {
+
+	private String sender;
+	private String receiver;
+	private String timestamp;
+	private double amount;
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		Transaction that = (Transaction) o;
+
+		if (Double.compare(that.getAmount(), getAmount()) != 0) {
+			return false;
+		}
+		if (!getSender().equals(that.getSender())) {
+			return false;
+		}
+		if (!getReceiver().equals(that.getReceiver())) {
+			return false;
+		}
+		return getTimestamp().equals(that.getTimestamp());
+	}
+
+	@Override
+	public int hashCode() {
+		int result;
+		long temp;
+		result = getSender().hashCode();
+		result = 31 * result + getReceiver().hashCode();
+		result = 31 * result + getTimestamp().hashCode();
+		temp = Double.doubleToLongBits(getAmount());
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+}
